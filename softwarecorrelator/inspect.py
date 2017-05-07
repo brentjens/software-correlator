b4fc837f720d5799026d314a4ad78ca6cb5fb180
Michiel Brentjens <brentjens@astron.nl>
2017-05-07 22:43:47 +0200

    Added inspect.data_loss_report_plot(): histogram of cumulative loss per SAP.

--- softwarecorrelator/inspect.py
+++ softwarecorrelator/inspect.py
@@ -1,6 +1,7 @@
 import numpy
 import h5py
 import logging
+import matplotlib.pyplot as plt
 
 from .utilities import working_dir
 from .lofarhdf5 import h5_structure, h5_complex_voltage_file_names
@@ -35,3 +36,23 @@ def data_loss_report_ascii(dir_name):
     for row in zip(file_names['x_re'], losses):
         print(row[0], '  %5.1f    %5.1f    %5.1f    %5.1f   %5.1f' % (tuple(numpy.array(row[1])*100) + (numpy.array(row[1]).max()*100,)))
     print('============================================================================')
+
+
+
+
+def data_loss_report_plot(dir_name):
+    file_names = h5_complex_voltage_file_names(dir_name)
+    losses = []
+    for pol in ['x_re', 'x_im', 'y_re', 'y_im']:
+        losses.append(numpy.array([data_loss_fraction(dir_name, file_name)
+                                   for file_name in sorted(file_names[pol])]))
+    losses = numpy.array(losses).T
+    max_losses = losses.sum(axis=1)
+    indices = numpy.arange(len(max_losses))
+    color_code = ['green', 'orange', 'red']+['red']*80
+    plt.barh(indices, max_losses*100, height=0.9, color=[color_code[int(loss/0.05)] for loss in max_losses])
+    plt.xlim(0, 100)
+    plt.yticks(indices, [name.split('_')[1][3:] for name in file_names['x_re']])
+    plt.xlabel('Percentage lost')
+    plt.ylabel('Antenna')
+    plt.title('Data loss %s: %.1f%% total' % (file_names['x_re'][0].split('_')[0], max_losses.mean()*100))
