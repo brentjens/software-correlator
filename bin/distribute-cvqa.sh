a1fd11f5b64459dd72117fdc970537255e1ee23c
Michiel Brentjens (CEP4) <brentjens@astron.nl>
2017-05-22 13:00:07 +0000

    Removed unnecessary -f in docker tag
    Docker on CPU nodes is much newer.

--- bin/distribute-cvqa.sh
+++ bin/distribute-cvqa.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
-IMG=cvqa.latest
-
+IMG=cvqa
+# -f option is only in docker version on head nodes, not cpu nodes which have newer version
 docker tag -f $IMG nexus.cep4.control.lofar:18080/$IMG
 docker push nexus.cep4.control.lofar:18080/$IMG
-
 # 12 is max num of cpu nodes to prevent hangs
 clush -f 12 -w cpu[01-50] docker pull nexus.cep4.control.lofar:18080/$IMG 
-clush -f 12 -w cpu[01-50] docker tag -f nexus.cep4.control.lofar:18080/$IMG $IMG
+clush -f 12 -w cpu[01-50] docker tag  nexus.cep4.control.lofar:18080/$IMG $IMG
