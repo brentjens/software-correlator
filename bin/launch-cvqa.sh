46faaf86d33cdf17c9dafa6dbe3972a58e8e3cde
Michiel Brentjens (CEP4) <brentjens@astron.nl>
2017-05-23 12:26:18 +0000

    Improved launch-cvqa.sh script.

--- bin/launch-cvqa.sh
+++ bin/launch-cvqa.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash
 
 SAS_ID=$1
+# -u userid/groupid lofarsys
 
-srun -N 1 docker run -it cvqa cvqa --output-dir="/data/projects/HOLOG_WINDMILL_TESTS" "/data/projects/HOLOG_WINDMILL_TESTS/$SAS_ID/cs/"
+#srun -N 1 docker run -v /data:/data:rw -u lofarsys:lofarsys --rm=true -t cvqa cvqa --output-dir="/data/projects/HOLOG_WINDMILL_TESTS" "/data/projects/HOLOG_WINDMILL_TESTS/$SAS_ID/cs/"
+
+#docker-run-slurm.sh 
+
+srun -N 1 -J "$USER-cvqa-$SAS_ID" \
+        docker run --rm -u 7149 \
+        -e USER=$USER -e HOME=$HOME \
+        -v /data:/data:rw \
+        -v /globalhome:/globalhome:rw \
+        --network host \
+        -t cvqa:latest cvqa \
+           --output-dir="/data/projects/HOLOG_WINDMILL_TESTS" \
+           "/data/projects/HOLOG_WINDMILL_TESTS/$SAS_ID/cs/"
 
