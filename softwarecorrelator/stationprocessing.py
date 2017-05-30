Reversed sign of FFT in PPF to obtain correct channel ordering.

softwarecorrelator/stationprocessing.py
@@ -1,8 +1,13 @@
 import numpy
+import multiprocessing as mp
 import scipy.fftpack as fft
 import scipy.signal as signal
 
-def fir_filter_coefficients(num_chan, num_taps, cal_factor=1./numpy.sqrt(1.8e1)):
+import h5py
+from .utilities import working_dir
+from .stationbandpass import lofar_station_subband_bandpass
+
+def fir_filter_coefficients(num_chan, num_taps, cal_factor=1./50.0):
     '''
     Compute FIR filter coefficients for channel separation.
 
@@ -34,7 +39,7 @@ def fir_filter_coefficients(num_chan, num_taps, cal_factor=1./numpy.sqrt(1.8e1))
     raw_coefficients = signal.firwin((num_taps)*num_chan, 1/(num_chan),
                                      width=0.5/(num_chan))
     auto_fftshift = raw_coefficients*(-1)**numpy.arange(num_taps*num_chan)
-    coefficients = numpy.array(auto_fftshift*(num_chan**1.5),
+    coefficients = numpy.array(auto_fftshift*(num_chan**0.5),
                                dtype=numpy.float32)
     coefficients *= cal_factor
     return coefficients.reshape((num_taps, num_chan))
@@ -80,27 +85,29 @@ def channelize_ppf(timeseries_taps, fir_coefficients):
     array([-0.03263591-0.01060404j, -0.00383157+0.00195229j,
            -0.00848089+0.02610143j,  0.78864020+1.54779351j], dtype=complex64)
     '''
-    return (fft.ifft((timeseries_taps*fir_coefficients).sum(axis=0)))
+    return (fft.fft((timeseries_taps*fir_coefficients).sum(axis=0)))
 
 
 
 def channelize_ppf_multi_ts(timeseries_taps, fir_coefficients):
     '''FIR coefficients are num_taps x num_chan, blocks are num_timeslots x num_taps x num_chan arrays'''
-    return (fft.ifft((timeseries_taps*fir_coefficients[numpy.newaxis,:,:]).sum(axis=1),
-                      axis=1))
+    return (fft.fft((timeseries_taps*fir_coefficients[numpy.newaxis,:,:]).sum(axis=1),
+                    axis=1))
 
 
 
 def channelize_ppf_contiguous_block(timeseries_taps, fir_coefficients):
     num_taps, num_chan = fir_coefficients.shape
-    num_ts_blocks = timeseries_taps//num_chan
+    num_ts_blocks = timeseries_taps.shape[0]
     num_spectra = num_ts_blocks -(num_taps-1)
     output_spectra = numpy.zeros((num_spectra, num_chan),
                                  dtype=numpy.complex64)
     for sp in range(num_spectra):
-        output_spectra[sp,:] += channelize_ppf(time_series_taps[sp:sp+num_taps,:],
+        output_spectra[sp,:] += channelize_ppf(timeseries_taps[sp:sp+num_taps,:],
                                                fir_coefficients)
     return output_spectra
+
+
 
 
 def samples_per_block(block_length_s, sample_duration_s, num_chan, num_taps):